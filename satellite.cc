47f48da59931a6fe054ffa12dbe26c0d807159b0
hansol2 <52481759+hansol2@users.noreply.github.com>
2025-03-27 17:05:33 +0900

    Update satellite.cc

--- satellite.cc
+++ satellite.cc
@@ -1,7 +1,7 @@
 #include "ns3/core-module.h"
 #include "ns3/network-module.h"
 #include "ns3/internet-module.h"
-#include "ns3/satellite-module.h" // SNS-3 전용 모듈
+#include "ns3/satellite-module.h"
 #include "ns3/applications-module.h"
 #include "ns3/mobility-module.h"
 #include "ns3/csma-module.h"
@@ -19,7 +19,7 @@ void GlobalPacketCapture(std::string context, Ptr<const T> packet) {
 
 void SetupGlobalPcapCapture() {
     PcapHelper pcapHelper;
-    globalPcapFile = pcapHelper.CreateFile("global_capture.pcap", std::ios::out, PcapHelper::DLT_EN10MB); // Ethernet 호환
+    globalPcapFile = pcapHelper.CreateFile("global_capture.pcap", std::ios::out, PcapHelper::DLT_EN10MB);
     Config::ConnectWithoutContext("/NodeList/*/DeviceList/*/Tx", MakeCallback(&GlobalPacketCapture<Packet>));
     Config::ConnectWithoutContext("/NodeList/*/DeviceList/*/Rx", MakeCallback(&GlobalPacketCapture<Packet>));
 }
@@ -28,48 +28,60 @@ int main(int argc, char *argv[]) {
     CommandLine cmd;
     cmd.Parse(argc, argv);
 
-    // 노드 생성
-    NodeContainer endUser1, ut, satellite, gw, endUser2;
-    endUser1.Create(1);
-    ut.Create(1);
-    satellite.Create(1);
-    gw.Create(1);
-    endUser2.Create(1);
-
-    // SNS-3 위성 헬퍼 설정
+    // SatHelper로 위성 네트워크 설정
     SatHelper satHelper;
-    satHelper.SetBeamNetworkType(SatHelper::GEO); // 정지궤도 위성
-    satHelper.CreatePredefinedScenario(SatHelper::SIMPLE); // 기본 시나리오 재사용 후 수정
+    satHelper.CreatePredefinedScenario(SatHelper::SIMPLE); // 기본 시나리오: UT, Satellite, GW 포함
+
+    // 노드 컨테이너 가져오기
+    NodeContainer utNodes = satHelper.UtNodes();
+    NodeContainer satNodes = satHelper.SatNodes();
+    NodeContainer gwNodes = satHelper.GwNodes();
+
+    // 추가 노드 생성 (endUser1, endUser2)
+    NodeContainer endUser1, endUser2;
+    endUser1.Create(1);
+    endUser2.Create(1);
 
     // 위치 설정
     MobilityHelper mobility;
     mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
     mobility.Install(endUser1);
     endUser1.Get(0)->GetObject<MobilityModel>()->SetPosition(Vector(0.0, 0.0, 0.0));
-    mobility.Install(ut);
-    ut.Get(0)->GetObject<MobilityModel>()->SetPosition(Vector(500.0, 0.0, 0.0));
-    mobility.Install(satellite);
-    satellite.Get(0)->GetObject<MobilityModel>()->SetPosition(Vector(0.0, 0.0, 36000000.0)); // GEO 36,000 km
-    mobility.Install(gw);
-    gw.Get(0)->GetObject<MobilityModel>()->SetPosition(Vector(1000.0, 0.0, 0.0));
+    mobility.Install(utNodes);
+    utNodes.Get(0)->GetObject<MobilityModel>()->SetPosition(Vector(500.0, 0.0, 0.0));
+    mobility.Install(satNodes);
+    satNodes.Get(0)->GetObject<MobilityModel>()->SetPosition(Vector(0.0, 0.0, 36000000.0));
+    mobility.Install(gwNodes);
+    gwNodes.Get(0)->GetObject<MobilityModel>()->SetPosition(Vector(1000.0, 0.0, 0.0));
     mobility.Install(endUser2);
     endUser2.Get(0)->GetObject<MobilityModel>()->SetPosition(Vector(2000.0, 0.0, 0.0));
 
-    // 유선 링크: endUser1 <-> ut
+    // 유선 링크: endUser1 <-> UT
     CsmaHelper csma;
     csma.SetChannelAttribute("DataRate", StringValue("100Mbps"));
     csma.SetChannelAttribute("Delay", StringValue("1ms"));
-    NetDeviceContainer devUserUt = csma.Install(NodeContainer(endUser1.Get(0), ut.Get(0)));
+    NetDeviceContainer devUserUt = csma.Install(NodeContainer(endUser1.Get(0), utNodes.Get(0)));
 
-    // 위성 링크: ut <-> satellite <-> gw (SNS-3 모듈 사용)
-    NetDeviceContainer devUtSat = satHelper.InstallUserLink(ut, satellite);
-    NetDeviceContainer devSatGw = satHelper.InstallGwLink(satellite, gw);
+    // SatHelper가 생성한 UT-Satellite, Satellite-GW 링크의 디바이스 가져오기
+    // SatHelper 내부에서 자동으로 설정된 디바이스를 직접 접근
+    Ptr<NetDevice> utSatDevice = utNodes.Get(0)->GetDevice(0); // UT의 첫 번째 디바이스
+    Ptr<NetDevice> satUtDevice = satNodes.Get(0)->GetDevice(0); // Satellite의 UT 연결 디바이스
+    Ptr<NetDevice> satGwDevice = satNodes.Get(0)->GetDevice(1); // Satellite의 GW 연결 디바이스
+    Ptr<NetDevice> gwSatDevice = gwNodes.Get(0)->GetDevice(0); // GW의 첫 번째 디바이스
 
-    // 유선 링크: gw <-> endUser2
+    NetDeviceContainer devUtSat;
+    devUtSat.Add(utSatDevice);
+    devUtSat.Add(satUtDevice);
+
+    NetDeviceContainer devSatGw;
+    devSatGw.Add(satGwDevice);
+    devSatGw.Add(gwSatDevice);
+
+    // 유선 링크: GW <-> endUser2
     PointToPointHelper p2p;
     p2p.SetDeviceAttribute("DataRate", StringValue("1Gbps"));
     p2p.SetChannelAttribute("Delay", StringValue("0.1ms"));
-    NetDeviceContainer devGwUser = p2p.Install(NodeContainer(gw.Get(0), endUser2.Get(0)));
+    NetDeviceContainer devGwUser = p2p.Install(NodeContainer(gwNodes.Get(0), endUser2.Get(0)));
 
     // 인터넷 스택 설치
     InternetStackHelper internet;
